c71c8bb6bb8fd509adb4b305e9966407473259f0
mukundsahu2802@gmail.com <mukundsahu2802@gmail.com>
2026-04-23 13:40:33 +0000

    commit errors

--- models/example/Marts/order_items.sql
+++ models/example/Marts/order_items.sql
@@ -7,7 +7,7 @@ select
     orders.order_date,
     {{ discounted_amt(
         'line_item.extended_price',
-        'line_item.discount_percentage'
+        'line_item.discount'
     ) }} as item_discount_amount
 from {{ ref('orders') }} as orders
 join {{ ref('lineitem') }} as line_item
